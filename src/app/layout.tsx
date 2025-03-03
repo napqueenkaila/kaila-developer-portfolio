91b5e3d065d171c4fe4e9d9f0885cfede28536d3
napqueenkaila <k.m.bullard14@gmail.com>
2025-03-03 16:10:12 -0500

    random changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -18,9 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-[#002525] text-[#f5efed]">
+    <html lang="en" className="bg-slate-900 text-[#f5efed]">
       <body className={clsx(urbanist.className, "relative min-h-screen")}>
+        <Header />
         {children}
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light" />
+        <Footer />
       </body>
     </html>
   );
