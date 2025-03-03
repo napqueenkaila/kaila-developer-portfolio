78193892b81fd13b004d65003f510ab88bd5a11d
napqueenkaila <k.m.bullard14@gmail.com>
2025-03-03 12:12:30 -0500

    fix type errors for Bounded and prismicio.ts

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<T extends React.ElementType = "section"> = {
+  as?: T;
   className?: string;
   children: React.ReactNode;
+} & React.ComponentProps<T>;
+
+const Bounded = <T extends React.ElementType = "section">({
+  as: Comp = "section",
+  className,
+  children,
+  ...restProps
+}: BoundedProps<T>) => {
+  return (
+    <Comp
+      className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+      {...restProps}
+    >
+      <div className="mx-auto w-full max-w-7xl">{children}</div>
+    </Comp>
+  );
 };
 
-const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
-  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
-    return (
-      <Comp
-        ref={ref}
-        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-        {...restProps}
-      >
-        <div className="mx-auto w-full max-w-7xl">{children}</div>
-      </Comp>
-    );
-  }
-);
 
 Bounded.displayName = "Bounded";
 
