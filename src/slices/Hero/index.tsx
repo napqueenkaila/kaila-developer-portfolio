86fb1e94c46501e66a2583552c8a25fd12e62b1a
napqueenkaila <k.m.bullard14@gmail.com>
2025-03-03 11:20:47 -0500

    init commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <h1>{ slice.primary.first_name }</h1>
+    </section>
+  );
+};
+
+export default Hero;
