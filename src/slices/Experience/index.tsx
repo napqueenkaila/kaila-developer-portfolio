e78db5df5110a51d83306f9d4e472d80fd236942
napqueenkaila <k.m.bullard14@gmail.com>
2025-03-06 11:19:09 -0500

    add /

--- src/slices/Experience/index.tsx
+++ src/slices/Experience/index.tsx
@@ -28,7 +28,7 @@ const Experience: FC<ExperienceProps> = ({ slice }) => {
           </Heading>
           <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
             <span>{item.time_period}</span>
-            <span className="text-3xl font-extralight"></span>
+            <span className="text-3xl font-extralight">/</span>
             <span>{item.workplace}</span>
           </div>
           <div className="prose prose-lg prose-invert mt-4">
